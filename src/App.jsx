8ea97fc1913c96e67d166e54a5d3dae5c9d2ae4f
Shivdeep Raina <shivdeepraina@gmail.com>
2024-06-03 18:49:04 +0530

    uploaded succefull

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
